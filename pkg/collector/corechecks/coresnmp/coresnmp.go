96fb90afc4232fb9d6d08486caa788a2b127ca33
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-10-27 16:16:56 +0100

    Add oid

--- pkg/collector/corechecks/coresnmp/coresnmp.go
+++ pkg/collector/corechecks/coresnmp/coresnmp.go
@@ -1,14 +1,15 @@
 package coresnmp
 
 import (
-
 	"github.com/DataDog/datadog-agent/pkg/aggregator"
 	"github.com/DataDog/datadog-agent/pkg/autodiscovery/integration"
 	"github.com/DataDog/datadog-agent/pkg/collector/check"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"time"
 
 	core "github.com/DataDog/datadog-agent/pkg/collector/corechecks"
 	yaml "gopkg.in/yaml.v2"
+	"github.com/soniah/gosnmp"
 )
 
 const (
@@ -50,6 +51,40 @@ func (c *CoresnmpCheck) Run() error {
 	sender, err := aggregator.GetSender(c.ID())
 	if err != nil {
 		return err
+	}
+
+	session := gosnmp.GoSNMP{
+		Target: "localhost",
+		Port:               uint16(1161),
+		Community:          "public",
+		Version:            gosnmp.Version2c,
+		Timeout:            time.Duration(2) * time.Second,
+		Retries:            3,
+		ExponentialTimeout: true,
+		MaxOids:            100,
+	}
+
+	err = session.Connect()
+	if err != nil {
+		log.Errorf("Connect() err: %v", err)
+	}
+	defer session.Conn.Close()
+
+	oids := []string{"1.3.6.1.2.1.25.6.3.1.5.130"}
+	result, err := session.Get(oids)
+	if err != nil {
+		log.Errorf("Get() err: %v", err)
+		return nil
+	}
+
+	for j, variable := range result.Variables {
+		log.Infof("%d: oid: %s ", j, variable.Name)
+		switch variable.Type {
+		case gosnmp.OctetString:
+			log.Infof("string: %s\n", string(variable.Value.([]byte)))
+		default:
+			log.Infof("number: %d\n", gosnmp.ToBigInt(variable.Value))
+		}
 	}
 
 	log.Debug("Run coresnmp")
