297beaadbc11cdc5a6fe5836c2848c5e895e5d6f
Kylian Serrania <kylian.serrania@datadoghq.com>
2020-10-29 19:39:53 +0100

    Test 3

--- omnibus/config/software/snowflake-connector-python-py3.rb
+++ omnibus/config/software/snowflake-connector-python-py3.rb
@@ -5,5 +5,5 @@ dependency "pip3"
 dependency "snowflake-connector-python"
 
 build do
-  command "#{install_dir}/embedded/bin/pip3 install #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-2.1.3"
+  command "cd #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-2.1.3 && #{install_dir}/embedded/bin/pip3 install ."
 end
