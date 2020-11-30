994fb50d17f534dd3096c0be6563f673693e110e
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-11-30 10:27:57 +0100

    Expand profiles

--- pkg/collector/corechecks/snmp/profile.go
+++ pkg/collector/corechecks/snmp/profile.go
@@ -10,33 +10,67 @@ import (
 type profileDefinitionMap map[string]profileDefinition
 
 type profileDefinition struct {
-	Metrics []metricsConfig `yaml:"metrics"`
+	Metrics    []metricsConfig   `yaml:"metrics"`
+	MetricTags []metricTagConfig `yaml:"metric_tags"`
+	Extends    []string          `yaml:"extends"`
 }
 
 func loadProfiles(pConfig profilesConfig) (profileDefinitionMap, error) {
 	// TODO: Profiles
 	//   - Load default profiles
 	//   - Load config profiles
-
-	confdPath := config.Datadog.GetString("confd_path")
 	profiles := make(map[string]profileDefinition)
 
 	for name, profile := range pConfig {
-		// TODO: Support profiles locations
-		// See https://github.com/DataDog/integrations-core/blob/d7f4d42a4721aea683056901cf2053395ff48173/snmp/datadog_checks/snmp/utils.py#L64-L73
-		filePath := confdPath + "/snmp.d/profiles/" + profile.DefinitionFile
+		definitionFile := profile.DefinitionFile
 
-		buf, err := ioutil.ReadFile(filePath)
+		profileDefinition, err := readProfileDefinition(definitionFile)
 		if err != nil {
 			return nil, err
 		}
 
-		profileDef := &profileDefinition{}
-		err = yaml.Unmarshal(buf, profileDef)
+		err = recursivelyExpandBaseProfiles(profileDefinition)
 		if err != nil {
-			return nil, fmt.Errorf("in file %q: %v", filePath, err)
+			return nil, err
 		}
-		profiles[name] = *profileDef
+
+		profiles[name] = *profileDefinition
 	}
 	return profiles, nil
+}
+
+func readProfileDefinition(definitionFile string) (*profileDefinition, error) {
+	filePath := resolveProfileDefinitionPath(definitionFile)
+	buf, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	profileDefinition := &profileDefinition{}
+	err = yaml.Unmarshal(buf, profileDefinition)
+	if err != nil {
+		return nil, fmt.Errorf("in file %q: %v", filePath, err)
+	}
+	return profileDefinition, nil
+}
+
+func resolveProfileDefinitionPath(definitionFile string) string {
+	// TODO: Support profiles locations
+	// See https://github.com/DataDog/integrations-core/blob/d7f4d42a4721aea683056901cf2053395ff48173/snmp/datadog_checks/snmp/utils.py#L64-L73
+	confdPath := config.Datadog.GetString("confd_path")
+	profilesPath := confdPath + "/snmp.d/profiles/"
+	return profilesPath + definitionFile
+}
+
+func recursivelyExpandBaseProfiles(definition *profileDefinition) error {
+	for _, basePath := range definition.Extends {
+		baseDefinition, err := readProfileDefinition(basePath)
+		if err != nil {
+			return err
+		}
+
+		definition.Metrics = append(definition.Metrics, baseDefinition.Metrics...)
+		definition.MetricTags = append(definition.MetricTags, baseDefinition.MetricTags...)
+	}
+	return nil
 }
