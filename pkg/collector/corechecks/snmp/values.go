e8c1f1a64a46d2070890722cf41d6b1d5aea0eac
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-11-30 18:33:08 +0100

    Fix log msg

--- pkg/collector/corechecks/snmp/values.go
+++ pkg/collector/corechecks/snmp/values.go
@@ -23,7 +23,7 @@ func (v *snmpValues) getColumnValues(oid string) (map[string]snmpValue, error) {
 	retValues := make(map[string]snmpValue)
 	values, ok := v.columnValues[oid]
 	if !ok {
-		return nil, fmt.Errorf("value for Scalar OID not found: %s", oid)
+		return nil, fmt.Errorf("value for Column OID not found: %s", oid)
 	}
 	for index, value := range values {
 		retValues[index] = value
