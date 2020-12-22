9caf33844c76ef882aaa9e1e25d0a9b2cf558774
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-12-22 16:03:24 +0100

    Update getValueFromPDU

--- pkg/collector/corechecks/snmp/gosnmp_utils.go
+++ pkg/collector/corechecks/snmp/gosnmp_utils.go
@@ -1,29 +1,43 @@
 package snmp
 
 import (
+	"github.com/DataDog/datadog-agent/pkg/util/log"
 	"github.com/soniah/gosnmp"
 	"strings"
 )
 
-func getValueFromPDU(pduVariable gosnmp.SnmpPDU) (string, snmpValue) {
+func getValueFromPDU(pduVariable gosnmp.SnmpPDU) (string, snmpValue, error) {
 	var value interface{}
 	name := strings.TrimLeft(pduVariable.Name, ".") // remove leading dot
+	// TODO: Support:
+	//   - gosnmp.Opaque ?
+	//     Seems opaque type is never returned https://github.com/gosnmp/gosnmp/blob/dc320dac5b53d95a366733fd95fb5851f2099387/helper.go#L195-L205
+	//   - gosnmp.Boolean: seems not exist anymore and not handled by gosnmp
 	switch pduVariable.Type {
-	case gosnmp.OctetString:
-		// TODO: Return custom val struct
+	case gosnmp.OctetString, gosnmp.BitString:
 		value = string(pduVariable.Value.([]byte))
-	default:
+	case gosnmp.Integer, gosnmp.Counter32, gosnmp.Gauge32, gosnmp.TimeTicks, gosnmp.Counter64, gosnmp.Uinteger32:
 		value = float64(gosnmp.ToBigInt(pduVariable.Value).Int64())
+	case gosnmp.OpaqueFloat:
+		value = float64(pduVariable.Value.(float32))
+	case gosnmp.OpaqueDouble:
+		value = pduVariable.Value.(float64)
+	default:
+		return name, snmpValue{}, log.Errorf("invalid type: %s", pduVariable.Type.String())
 	}
 	valueType := gosnmpTypeToSimpleType(pduVariable.Type)
-	return name, snmpValue{valType: valueType, val: value}
+	return name, snmpValue{valType: valueType, val: value}, nil
 }
 
 func resultToScalarValues(result *gosnmp.SnmpPacket) (values map[string]snmpValue) {
 	// TODO: test me
 	returnValues := make(map[string]snmpValue)
 	for _, pduVariable := range result.Variables {
-		name, value := getValueFromPDU(pduVariable)
+		name, value, err := getValueFromPDU(pduVariable)
+		if err != nil {
+			log.Debugf("Cannot get value for variable `%v` with type `%v` and value `%v`", pduVariable.Name, pduVariable.Type, pduVariable.Value)
+			continue
+		}
 		returnValues[name] = value
 	}
 	return returnValues
@@ -34,7 +48,11 @@ func resultToColumnValues(columnOids []string, result *gosnmp.SnmpPacket) (map[s
 	returnValues := make(map[string]map[string]snmpValue)
 	nextOidsMap := make(map[string]string)
 	for i, pduVariable := range result.Variables {
-		oid, value := getValueFromPDU(pduVariable)
+		oid, value, err := getValueFromPDU(pduVariable)
+		if err != nil {
+			log.Debugf("Cannot get value for variable `%v` with type `%v` and value `%v`", pduVariable.Name, pduVariable.Type, pduVariable.Value)
+			continue
+		}
 		columnOid := columnOids[i%len(columnOids)]
 		if _, ok := returnValues[columnOid]; !ok {
 			returnValues[columnOids[i]] = make(map[string]snmpValue)
--- pkg/collector/corechecks/snmp/gosnmp_utils_test.go
+++ pkg/collector/corechecks/snmp/gosnmp_utils_test.go
@@ -1,1 +1,181 @@
 package snmp
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"github.com/soniah/gosnmp"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_getValueFromPDU(t *testing.T) {
+	tests := []struct {
+		caseName          string
+		pduVariable       gosnmp.SnmpPDU
+		expectedName      string
+		expectedSnmpValue snmpValue
+		expectedErr       error
+	}{
+		{
+			"Name",
+			gosnmp.SnmpPDU{
+				Name:  ".1.2.3",
+				Type:  gosnmp.Integer,
+				Value: 141,
+			},
+			"1.2.3",
+			snmpValue{valType: Other, val: float64(141)},
+			nil,
+		},
+		{
+			"OctetString",
+			gosnmp.SnmpPDU{
+				Name:  ".1.2.3",
+				Type:  gosnmp.Integer,
+				Value: 141,
+			},
+			"1.2.3",
+			snmpValue{valType: Other, val: float64(141)},
+			nil,
+		},
+		{
+			"OctetString",
+			gosnmp.SnmpPDU{
+				Name:  ".1.2.3",
+				Type:  gosnmp.OctetString,
+				Value: []byte(`myVal`),
+			},
+			"1.2.3",
+			snmpValue{valType: Other, val: "myVal"},
+			nil,
+		},
+		{
+			"BitString",
+			gosnmp.SnmpPDU{
+				Name:  ".1.2.3",
+				Type:  gosnmp.BitString,
+				Value: []byte(`myVal`),
+			},
+			"1.2.3",
+			snmpValue{valType: Other, val: "myVal"},
+			nil,
+		},
+		{
+			"Null",
+			gosnmp.SnmpPDU{
+				Name:  ".1.2.3",
+				Type:  gosnmp.Null,
+				Value: nil,
+			},
+			"1.2.3",
+			snmpValue{},
+			log.Errorf("invalid type: Null"),
+		},
+		{
+			"Counter32",
+			gosnmp.SnmpPDU{
+				Name:  ".1.2.3",
+				Type:  gosnmp.Counter32,
+				Value: uint(10),
+			},
+			"1.2.3",
+			snmpValue{valType: Counter, val: float64(10)},
+			nil,
+		},
+		{
+			"Gauge32",
+			gosnmp.SnmpPDU{
+				Name:  ".1.2.3",
+				Type:  gosnmp.Gauge32,
+				Value: uint(10),
+			},
+			"1.2.3",
+			snmpValue{valType: Other, val: float64(10)},
+			nil,
+		},
+		{
+			"TimeTicks",
+			gosnmp.SnmpPDU{
+				Name:  ".1.2.3",
+				Type:  gosnmp.TimeTicks,
+				Value: uint32(10),
+			},
+			"1.2.3",
+			snmpValue{valType: Other, val: float64(10)},
+			nil,
+		},
+		{
+			"Counter64",
+			gosnmp.SnmpPDU{
+				Name:  ".1.2.3",
+				Type:  gosnmp.Counter64,
+				Value: uint64(10),
+			},
+			"1.2.3",
+			snmpValue{valType: Counter, val: float64(10)},
+			nil,
+		},
+		{
+			"Uinteger32",
+			gosnmp.SnmpPDU{
+				Name:  ".1.2.3",
+				Type:  gosnmp.Uinteger32,
+				Value: uint32(10),
+			},
+			"1.2.3",
+			snmpValue{valType: Other, val: float64(10)},
+			nil,
+		},
+		{
+			"OpaqueFloat",
+			gosnmp.SnmpPDU{
+				Name:  ".1.2.3",
+				Type:  gosnmp.OpaqueFloat,
+				Value: float32(10),
+			},
+			"1.2.3",
+			snmpValue{valType: Other, val: float64(10)},
+			nil,
+		},
+		{
+			"OpaqueDouble",
+			gosnmp.SnmpPDU{
+				Name:  ".1.2.3",
+				Type:  gosnmp.OpaqueDouble,
+				Value: float64(10),
+			},
+			"1.2.3",
+			snmpValue{valType: Other, val: float64(10)},
+			nil,
+		},
+		{
+			"NoSuchObject",
+			gosnmp.SnmpPDU{
+				Name:  ".1.2.3",
+				Type:  gosnmp.NoSuchObject,
+				Value: nil,
+			},
+			"1.2.3",
+			snmpValue{},
+			log.Errorf("invalid type: NoSuchObject"),
+		},
+		{
+			"NoSuchInstance",
+			gosnmp.SnmpPDU{
+				Name:  ".1.2.3",
+				Type:  gosnmp.NoSuchInstance,
+				Value: nil,
+			},
+			"1.2.3",
+			snmpValue{},
+			log.Errorf("invalid type: NoSuchInstance"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.caseName, func(t *testing.T) {
+			name, value, err := getValueFromPDU(tt.pduVariable)
+			assert.Equal(t, name, tt.expectedName)
+			assert.Equal(t, value, tt.expectedSnmpValue)
+			assert.Equal(t, err, tt.expectedErr)
+		})
+	}
+}
