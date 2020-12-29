81b682baba0cc137d8708c3107872c4c44fee40b
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-12-29 12:01:19 +0100

    Add getMostSpecificOid

--- pkg/collector/corechecks/snmp/profile.go
+++ pkg/collector/corechecks/snmp/profile.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -118,4 +119,51 @@ func recursivelyExpandBaseProfiles(definition *profileDefinition, extends []stri
 		}
 	}
 	return nil
+}
+
+func getOidPatternSpecificity(pattern string) ([]int, error) {
+	wildcardKey := -1
+	var parts []int
+	for _, part := range strings.Split(strings.TrimLeft(pattern, "."), ".") {
+		if part == "*" {
+			parts = append(parts, wildcardKey)
+		} else {
+			intPart, err := strconv.Atoi(part)
+			if err != nil {
+				return nil, fmt.Errorf("error parsing part `%s` for pattern `%s`: %v", part, pattern, err)
+			}
+			parts = append(parts, intPart)
+		}
+	}
+	return parts, nil
+}
+
+func getMostSpecificOid(oids []string) (string, error) {
+	var mostSpecificParts []int
+	var mostSpecificOid string
+
+	if len(oids) == 0 {
+		return "", fmt.Errorf("cannot get most specific oid from empty list of oids")
+	}
+
+	for _, oid := range oids {
+		parts, err := getOidPatternSpecificity(oid)
+		if err != nil {
+			return "", err
+		}
+		if len(parts) > len(mostSpecificParts) {
+			mostSpecificParts = parts
+			mostSpecificOid = oid
+			continue
+		}
+		if len(parts) == len(mostSpecificParts) {
+			for i := range mostSpecificParts {
+				if parts[i] > mostSpecificParts[i] {
+					mostSpecificParts = parts
+					mostSpecificOid = oid
+				}
+			}
+		}
+	}
+	return mostSpecificOid, nil
 }
