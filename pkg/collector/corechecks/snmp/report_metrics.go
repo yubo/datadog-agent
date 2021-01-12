f6300e168b7b4a62d819d54e2dda3a2c77be9d16
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-01-12 12:24:11 +0100

    Copy tags before sort

--- pkg/collector/corechecks/snmp/report_metrics.go
+++ pkg/collector/corechecks/snmp/report_metrics.go
@@ -63,11 +63,13 @@ func (ms *metricSender) reportColumnMetrics(metricConfig metricsConfig, values *
 	}
 }
 
-func (ms *metricSender) sendMetric(metricName string, value snmpValue, tags []string, forcedType string, options metricsConfigOption) {
+func (ms *metricSender) sendMetric(metricName string, value snmpValue, srcTags []string, forcedType string, options metricsConfigOption) {
 	// TODO: Submit using the right type
 	//   See https://github.com/DataDog/integrations-core/blob/d6add1dfcd99c3610f45390b8d4cd97390af1f69/snmp/datadog_checks/snmp/pysnmp_inspect.py#L34-L48
 	metricFullName := "snmp." + metricName
 
+	var tags []string
+	tags = append(tags, srcTags...)
 	sort.Strings(tags)
 
 	if forcedType != "" {
