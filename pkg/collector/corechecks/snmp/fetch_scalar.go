dde4ef3bce50b68ef3cb17b19b914b81614bf27b
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-01-19 15:23:38 +0100

    Refactor fetch scalar

--- pkg/collector/corechecks/snmp/fetch_scalar.go
+++ pkg/collector/corechecks/snmp/fetch_scalar.go
@@ -28,6 +28,14 @@ func fetchScalarOidsWithBatching(session sessionAPI, oids []string, oidBatchSize
 }
 
 func fetchScalarOids(session sessionAPI, oids []string) (scalarResultValuesType, error) {
+	values, err := doFetchScalarOids(session, oids)
+	if err != nil {
+		return nil, err
+	}
+	return values, nil
+}
+
+func doFetchScalarOids(session sessionAPI, oids []string) (scalarResultValuesType, error) {
 	// Get results
 	log.Debugf("fetchScalarOidsWithBatching() oids: %v", oids)
 	results, err := session.Get(oids)
@@ -36,9 +44,15 @@ func fetchScalarOids(session sessionAPI, oids []string) (scalarResultValuesType,
 		return nil, fmt.Errorf("error getting oids: %s", err.Error())
 	}
 	values := resultToScalarValues(results)
+	retryScalarOids(session, results, values)
+	return values, nil
+}
 
-	// Retry on NoSuchObject or NoSuchInstance for scalar oids not ending with .0
-	// This helps keeping compatibility with python implementation.
+// retryScalarOids retries on NoSuchObject or NoSuchInstance for scalar oids not ending with `.0`.
+// This helps keeping compatibility with python implementation.
+// This is not need in normal circumstances where scalar OIDs end with `.0`.
+// If the oid does not end with `.0`, we will retry by appending `.0` to it.
+func retryScalarOids(session sessionAPI, results *gosnmp.SnmpPacket, valuesToUpdate scalarResultValuesType) {
 	retryOids := make(map[string]string)
 	for _, variable := range results.Variables {
 		oid := variable.Name
@@ -58,10 +72,9 @@ func fetchScalarOids(session sessionAPI, oids []string) (scalarResultValuesType,
 			retryValues := resultToScalarValues(retryResults)
 			for initialOid, actualOid := range retryOids {
 				if value, ok := retryValues[actualOid]; ok {
-					values[initialOid] = value
+					valuesToUpdate[initialOid] = value
 				}
 			}
 		}
 	}
-	return values, nil
 }
