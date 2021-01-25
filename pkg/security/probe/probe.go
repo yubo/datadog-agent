633402b3dc8f6cab2b864b081121b9ae64f225c1
Sylvain Baubeau <lebauce@gmail.com>
2021-01-25 13:27:02 +0100

    Add parent inode support to inodeDiscarders

--- pkg/security/probe/probe.go
+++ pkg/security/probe/probe.go
@@ -20,7 +20,6 @@ import (
 	"github.com/DataDog/datadog-go/statsd"
 	lib "github.com/DataDog/ebpf"
 	"github.com/DataDog/ebpf/manager"
-	"github.com/hashicorp/golang-lru/simplelru"
 	"github.com/pkg/errors"
 
 	"github.com/DataDog/datadog-agent/pkg/ebpf/bytecode"
@@ -65,8 +64,6 @@ type Probe struct {
 	// Approvers / discarders section
 	pidDiscarders      *pidDiscarders
 	inodeDiscarders    *inodeDiscarders
-	invalidDiscarders  map[eval.Field]map[interface{}]bool
-	regexCache         *simplelru.LRU
 	flushingDiscarders int64
 	approvers          map[eval.EventType]activeApprovers
 }
@@ -180,7 +177,9 @@ func (p *Probe) Init(client *statsd.Client) error {
 		return err
 	}
 
-	p.inodeDiscarders = newInodeDiscarders(inodeDiscardersMap, discarderRevisionsMap)
+	if p.inodeDiscarders, err = newInodeDiscarders(inodeDiscardersMap, discarderRevisionsMap, p.resolvers.DentryResolver); err != nil {
+		return err
+	}
 
 	if err := p.resolvers.Start(p.ctx); err != nil {
 		return err
@@ -685,16 +684,6 @@ func (p *Probe) Close() error {
 	return p.manager.Stop(manager.CleanAll)
 }
 
-// IsInvalidDiscarder returns whether the given value is a valid discarder for the given field
-func (p *Probe) IsInvalidDiscarder(field eval.Field, value interface{}) bool {
-	values, exists := p.invalidDiscarders[field]
-	if !exists {
-		return false
-	}
-
-	return values[value]
-}
-
 // GetDebugStats returns the debug stats
 func (p *Probe) GetDebugStats() map[string]interface{} {
 	debug := map[string]interface{}{
@@ -715,22 +704,15 @@ func (p *Probe) NewRuleSet(opts *rules.Opts) *rules.RuleSet {
 
 // NewProbe instantiates a new runtime security agent probe
 func NewProbe(config *config.Config, client *statsd.Client) (*Probe, error) {
-	regexCache, err := simplelru.NewLRU(64, nil)
-	if err != nil {
-		return nil, err
-	}
-
 	ctx, cancel := context.WithCancel(context.Background())
 
 	p := &Probe{
-		config:            config,
-		invalidDiscarders: getInvalidDiscarders(),
-		approvers:         make(map[eval.EventType]activeApprovers),
-		managerOptions:    ebpf.NewDefaultOptions(),
-		regexCache:        regexCache,
-		ctx:               ctx,
-		cancelFnc:         cancel,
-		statsdClient:      client,
+		config:         config,
+		approvers:      make(map[eval.EventType]activeApprovers),
+		managerOptions: ebpf.NewDefaultOptions(),
+		ctx:            ctx,
+		cancelFnc:      cancel,
+		statsdClient:   client,
 	}
 	p.detectKernelVersion()
 
