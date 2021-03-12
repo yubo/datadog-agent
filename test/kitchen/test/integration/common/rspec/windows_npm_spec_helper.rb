6d2d6b76f08850752e7c3c54440275c70ec09f4e
Kylian Serrania <kylian.serrania@datadoghq.com>
2021-03-12 10:36:12 +0100

    [kitchen] Remove process_config setup for NPM for Windows

--- test/kitchen/test/integration/common/rspec/windows_npm_spec_helper.rb
+++ test/kitchen/test/integration/common/rspec/windows_npm_spec_helper.rb
@@ -53,20 +53,6 @@ end
 
 shared_examples_for 'a Windows Agent with NPM running' do
   it 'can start system probe' do
-    conf_path = ""
-    if os != :windows
-      conf_path = "/etc/datadog-agent/datadog.yaml"
-    else
-      conf_path = "#{ENV['ProgramData']}\\Datadog\\datadog.yaml"
-    end
-    f = File.read(conf_path)
-    confYaml = YAML.load(f)
-    if !confYaml.key("process_config")
-      confYaml["process_config"] = {}
-    end
-    confYaml["process_config"]["enabled"] = "true"
-    File.write(conf_path, confYaml.to_yaml)
-
     if os != :windows
       spconf_path = "/etc/datadog-agent/datadog.yaml"
     else
