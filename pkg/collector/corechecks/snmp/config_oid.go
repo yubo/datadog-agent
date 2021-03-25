3da4b792ee5cc7c112d8e337a14594ad91fceca5
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-03-25 16:02:18 +0100

    Add test me

--- pkg/collector/corechecks/snmp/config_oid.go
+++ pkg/collector/corechecks/snmp/config_oid.go
@@ -6,24 +6,29 @@ type oidConfig struct {
 }
 
 func (oc *oidConfig) hasOids() bool {
+	// TODO: Test me
 	return len(oc.columnOids) != 0 || len(oc.scalarOids) != 0
 }
 
 func (oc *oidConfig) addScalarOid(oidToAdd string) {
+	// TODO: Test me
 	oc.scalarOids = appendOidIfMissing(oc.scalarOids, oidToAdd)
 }
 
 func (oc *oidConfig) addScalarOids(oidsToAdd []string) {
+	// TODO: Test me
 	for _, oid := range oidsToAdd {
 		oc.addScalarOid(oid)
 	}
 }
 
 func (oc *oidConfig) addColumnOid(oidToAdd string) {
+	// TODO: Test me
 	oc.columnOids = appendOidIfMissing(oc.columnOids, oidToAdd)
 }
 
 func (oc *oidConfig) addColumnOids(oidsToAdd []string) {
+	// TODO: Test me
 	for _, oid := range oidsToAdd {
 		oc.addColumnOid(oid)
 	}
