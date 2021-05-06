b8cd7588cdb96f1bf649bd867ced342dff6286d1
Kylian Serrania <kylian.serrania@datadoghq.com>
2021-05-06 16:52:25 +0200

    Create package dir before using it

--- omnibus/config/software/datadog-agent.rb
+++ omnibus/config/software/datadog-agent.rb
@@ -108,6 +108,7 @@ build do
   else
     copy 'bin/agent/ddtray.exe', "#{install_dir}/bin/agent"
     copy 'bin/agent/dist', "#{install_dir}/bin/agent"
+    mkdir "#{Omnibus::Config.package_dir()}"
     copy 'bin/agent/customaction.pdb', "#{Omnibus::Config.package_dir()}"
   end
 
