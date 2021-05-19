db3bb02d4a15718f26c6c8e17346bd2d1ce6057b
Isabelle Sauve <isabelle.sauve@datadoghq.com>
2021-05-18 23:34:04 -0400

    Add kernel header fetching telemetry

--- pkg/network/event_common.go
+++ pkg/network/event_common.go
@@ -105,6 +105,7 @@ type ConnectionsTelemetry struct {
 type RuntimeCompilationTelemetry struct {
 	RuntimeCompilationEnabled  bool
 	RuntimeCompilationResult   int32
+	KernelHeaderFetchResult    int32
 	RuntimeCompilationDuration int64
 }
 
