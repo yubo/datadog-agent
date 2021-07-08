8ceff0c6dda060efeb9f1642f091bc6db84b4735
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2021-07-07 22:44:39 -0400

    [kitchen] debian workaround: add hosts entry to operate properly across regions

--- test/kitchen/site-cookbooks/dd-agent-debian-workaround/metadata.rb
+++ test/kitchen/site-cookbooks/dd-agent-debian-workaround/metadata.rb
@@ -1,5 +1,6 @@
 name             "dd-agent-debian-workaround"
 maintainer       "Datadog"
-description      "Works around dirmngr not being installed"
+description      "Works around debian specific issues"  # dirmgr not installed + hostname resolution
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
+depends          "hostsfile"
--- test/kitchen/site-cookbooks/dd-agent-debian-workaround/recipes/default.rb
+++ test/kitchen/site-cookbooks/dd-agent-debian-workaround/recipes/default.rb
@@ -7,7 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe "hostsfile"
+
 if node['platform_family'] == 'debian'
+  hostsfile_entry '127.0.0.1' do
+    hostname  node['hostname']
+    aliases   ['localhost']
+    action    :append
+  end
+
   package 'install-dirmngr' do
     package_name 'dirmngr'
     action :install
