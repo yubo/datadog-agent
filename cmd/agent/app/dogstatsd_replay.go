1a935f0157b735eb0fad75aff441c7267be1feec
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2021-07-29 02:45:53 +0200

    [dogstatsd] replay: allow for multiple iteration replay

--- cmd/agent/app/dogstatsd_replay.go
+++ cmd/agent/app/dogstatsd_replay.go
@@ -29,13 +29,19 @@ import (
 )
 
 var (
-	dsdReplayFilePath string
-	dsdTaggerFilePath string
+	dsdReplayFilePath   string
+	dsdTaggerFilePath   string
+	dsdReplayIterations int
+)
+
+const (
+	defaultIterations = 1
 )
 
 func init() {
 	AgentCmd.AddCommand(dogstatsdReplayCmd)
 	dogstatsdReplayCmd.Flags().StringVarP(&dsdReplayFilePath, "file", "f", "", "Input file with TCP traffic to replay.")
+	dogstatsdReplayCmd.Flags().IntVarP(&dsdReplayIterations, "loops", "l", defaultIterations, "Number of iterationsi to replay.")
 	dogstatsdReplayCmd.Flags().StringVarP(&dsdTaggerFilePath, "tagger", "t", "", "Input file with TCP traffic to replay.")
 }
 
@@ -79,6 +85,7 @@ func dogstatsdReplay() error {
 	}()
 
 	fmt.Printf("Replaying dogstatsd traffic...\n\n")
+
 	// TODO: refactor all the instantiation of the SecureAgentClient to a helper
 	token, err := security.FetchAuthToken()
 	if err != nil {
@@ -117,6 +124,7 @@ func dogstatsdReplay() error {
 	}
 
 	if err != nil {
+		fmt.Printf("could not open: %s\n", dsdReplayFilePath)
 		return err
 	}
 
@@ -168,22 +176,27 @@ func dogstatsdReplay() error {
 	// wait for go routine to start processing...
 	time.Sleep(time.Second)
 
-replay:
-	for {
-		select {
-		case msg := <-reader.Traffic:
-			// The cadence is enforced by the reader. The reader will only write to
-			// the traffic channel when it estimates the payload should be submitted.
-			n, oobn, err := conn.(*net.UnixConn).WriteMsgUnix(
-				msg.Payload[:msg.PayloadSize], replay.GetUcredsForPid(msg.Pid), addr)
-			if err != nil {
-				return err
+iterate:
+	for i := 0; i < dsdReplayIterations || dsdReplayIterations == 0; i++ {
+
+	replay:
+		for {
+			select {
+			case msg := <-reader.Traffic:
+				// The cadence is enforced by the reader. The reader will only write to
+				// the traffic channel when it estimates the payload should be submitted.
+				n, oobn, err := conn.(*net.UnixConn).WriteMsgUnix(
+					msg.Payload[:msg.PayloadSize], replay.GetUcredsForPid(msg.Pid), addr)
+				if err != nil {
+					return err
+				}
+				fmt.Printf("Sent Payload: %d bytes, and OOB: %d bytes\n", n, oobn)
+			case <-reader.Done:
+				reader.Seek(0)
+				break replay
+			case <-done:
+				break iterate
 			}
-			fmt.Printf("Sent Payload: %d bytes, and OOB: %d bytes\n", n, oobn)
-		case <-reader.Done:
-			break replay
-		case <-done:
-			break replay
 		}
 	}
 
